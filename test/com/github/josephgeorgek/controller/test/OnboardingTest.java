972d7743f28951edf6376383307c6dbedfb45cfd
passionfish <josephgeorgek@gmail.com>
2015-10-10 17:26:35 +0800

    Onboarding

--- test/com/github/josephgeorgek/controller/test/OnboardingTest.java
+++ test/com/github/josephgeorgek/controller/test/OnboardingTest.java
@@ -1,6 +1,7 @@
 package com.github.josephgeorgek.controller.test;
 
 import static org.junit.Assert.*;
+import com.github.josephgeorgek.controller.Onboarding;;
 
 import org.junit.Test;
 
@@ -12,13 +13,13 @@ public class OnboardingTest {
 	}
 	@Test
 	  public void testFoo() throws Exception {
-	    One one = new One();
+		Onboarding one = new Onboarding();
 	    assertEquals("foo", one.foo());
 	  }
 
 	  @Test
 	  public void testBoth() throws Exception {
-	    One one = new One();
+		  Onboarding one = new Onboarding();
 	    assertEquals("toto", one.toto());
 	    assertEquals("foo", one.foo());
 	  }
